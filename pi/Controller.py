704b2cd80baf8d14d515a391905ecd2eb71f2c2a
Fatih Eser <afeserr@gmail.com>
2019-11-15 23:00:27 +0300

    Add missing controller.

---
+++ pi/Controller.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import Server
+import Constants
+import cv2
+
+import numpy as np
+
+'''
+Server side
+
+Currently under test...
+
+'''
+
+constants   = Constants.getConstants()
+
+videoServer   = Server.VideoServer(constants)
+# CommandClient = pi.src.Client.CommandClient()
+
+
+# Below are some helper functions for scripting, not related to the Controller directly!
+def saveImage(imName):
+    pil_img     = videoServer.receiveFrame()
+    numpy_image = np.array(pil_img)
+    frame         = opencv_image=cv2.cvtColor(numpy_image, cv2.COLOR_RGB2BGR)
+    cv2.imwrite(str(imName) + '.jpg', frame)

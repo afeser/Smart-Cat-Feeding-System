e3e7aa3b8179a5008d195117194e6a020d9282e2
Fatih Eser <afeserr@gmail.com>
2020-02-16 16:56:14 +0300

    Update identifier, - Move remove duplicates out of the import function, it will be called manually from now on. - Fix syntax in getCatName

--- cv/src/Identifier.py
+++ cv/src/Identifier.py
@@ -227,6 +227,35 @@ class Identifier:
             im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
             localImages[basename].append(im)
 
+        def eliminateDuplicateVectors(self):
+            '''
+            Since it requires a lot of time, importing directory does not call
+            eliminate duplicate vectors from database.
+            It can be done by manually calling this function.
+
+            It eliminates for every class...
+            '''
+
+            for catName in self._database:
+                logging.debug('Eliminating the duplicates')
+                self.debugTime(reset=True)
+                eliminated = 0
+                # Eliminate duplicates
+                eliminateThose = []
+                for index1, vector1 in enumerate(self._database[catName]):
+                    for index2, vector2 in enumerate(self._database[catName]):
+                        if index1 != index2 and np.array_equal(vector1, vector2):
+                            eliminateThose.append(index1)
+                            eliminated = eliminated + 1
+
+                eliminateThose.reverse()
+                for el in eliminateThose:
+                    logging.debug('Removing duplicated at index ' + str(el))
+                    self._database.pop(el)
+
+                self.debugTime('eliminating duplicates')
+                logging.debug('Eliminated duplicates ' + str(eliminated))
+
 
         def _createSiftVectorsFromImageSet(images, catName):
             '''
@@ -287,23 +316,6 @@ class Identifier:
                             plt.close(new_fig)
 
 
-                logging.debug('Eliminating the duplicates')
-                self.debugTime(reset=True)
-                eliminated = 0
-                # Eliminate duplicates
-                eliminateThose = []
-                for index1, vector1 in enumerate(completeVectors):
-                    for index2, vector2 in enumerate(completeVectors):
-                        if index1 != index2 and np.array_equal(vector1, vector2):
-                            eliminateThose.append(index1)
-                            eliminated = eliminated + 1
-
-                eliminateThose.reverse()
-                for el in eliminateThose:
-                    completeVectors.pop(el)
-                    
-                self.debugTime('eliminating duplicates')
-                logging.debug('Eliminated duplicates ' + str(eliminated))
 
                 return completeVectors
 
@@ -325,12 +337,6 @@ class Identifier:
                                 completeVectors.append(siftVectors1[match[0].queryIdx])
                                 completeVectors.append(siftVectors2[match[0].trainIdx])
 
-
-            # Eliminate duplicates
-            for index1, vector1 in enumerate(completeVectors):
-                for index2, vector2 in enumerate(completeVectors):
-                    if index1 != index2 and np.array_equal(vector1, vector2):
-                        completeVectors.pop(index1)
 
             return completeVectors
 

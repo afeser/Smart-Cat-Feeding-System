f0b817c4efa127d68b383aa4d0aaf474e83a60d1
furkan <furkanaldemir@gmail.com>
2020-02-23 14:31:59 -0500

    Initialize web branch

---
+++ web/app/models.py
@@ -0,0 +1,56 @@
+from datetime import datetime
+from app import db, login
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(128), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    devices = db.relationship('Device', backref='owner', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+class Device(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    location = db.Column(db.String(128), index=True, default='location')
+    battery_percentage = db.Column(db.Integer, default=100)
+    food_percentage = db.Column(db.Integer,default=100)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    cats = db.relationship('Cat',backref='kitchen', lazy='dynamic')
+
+    def set_location(self, location):
+        self.location = location
+
+    def set_battery(self, battery):
+        self.battery = battery
+
+    def set_food_level(self, food_level):
+        self.food_level = food_level
+
+    def __repr__(self):
+        return '<Device {}>'.format(self.location)
+
+class Cat(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, default='name')
+    last_feeding_time = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    device_id = db.Column(db.Integer, db.ForeignKey('device.id'))
+
+    def set_name(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<Cat {}>'.format(self.name)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
---
+++ web/app/routes.py
@@ -0,0 +1,64 @@
+from flask import render_template, flash, redirect, url_for, request
+from app import app
+from app.forms import LoginForm
+from app.models import User, Device, Cat
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
+
+@app.route('/')
+@app.route('/index/')
+@login_required
+def index():
+    return render_template('index.html', title='Home')
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout/')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    devices = user.devices
+    device_ids = []
+    for device in devices:
+        device_ids.append(device.id)
+    return render_template('user.html', devices=devices, device_ids=device_ids)
+
+@app.route('/user/<username>/<device_id>')
+@login_required
+def device(username,device_id):
+    user = User.query.filter_by(username=username).first_or_404()
+    device = user.devices.filter_by(id=device_id).first()
+    cats = device.cats
+    cat_ids = []
+    for cat in cats:
+        cat_ids.append(cat.id)
+    return render_template('device.html', device=device, cats=cats,cat_ids=cat_ids)
+
+@app.route('/user/<username>/<device_id>/<cat_id>')
+@login_required
+def cat(username,device_id,cat_id):
+    user = User.query.filter_by(username=username).first_or_404()
+    print(device_id)
+    device = user.devices.filter_by(id=device_id).first()
+    cat = device.cats.filter_by(id=cat_id).first()
+    return render_template('cat.html', device=device, cat=cat)

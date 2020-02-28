9e5e38e0277db47b6b63b7ea8d9a280289318178
Fatih Eser <afeserr@gmail.com>
2020-02-28 18:37:48 +0300

    Write colorSpaceTrial

--- cv/test/colorSpaceTrial.py
+++ cv/test/colorSpaceTrial.py
@@ -4,16 +4,31 @@ import cv2
 
 histogramType = 'HSV'
 
+bound=list(range(0,255,10))
 
-plt.figure(figsize=(16,12)); plt.title('Ceren 1 ' + histogramType + ' Histogram'); plt.xlabel('Data Points'); plt.ylabel('Occurrences')
-plt.hist(cv2.cvtColor(cv2.imread('ceren_1.jpg'), cv2.COLOR_BGR2HSV)[:,:,0].reshape(-1), bins=20, alpha=0.3)
-plt.figure(figsize=(16,12)); plt.title('Ceren 2 ' + histogramType + ' Histogram'); plt.xlabel('Data Points'); plt.ylabel('Occurrences')
-plt.hist(cv2.cvtColor(cv2.imread('ceren_2.jpg'), cv2.COLOR_BGR2HSV)[:,:,0].reshape(-1), bins=20, alpha=0.3)
-plt.figure(figsize=(16,12)); plt.title('Esra 1 ' + histogramType + ' Histogram'); plt.xlabel('Data Points'); plt.ylabel('Occurrences')
-plt.hist(cv2.cvtColor(cv2.imread('esra_1.jpg'), cv2.COLOR_BGR2HSV)[:,:,0].reshape(-1), bins=20, alpha=0.3)
-plt.figure(figsize=(16,12)); plt.title('Esra 2 ' + histogramType + ' Histogram'); plt.xlabel('Data Points'); plt.ylabel('Occurrences')
-plt.hist(cv2.cvtColor(cv2.imread('esra_2.jpg'), cv2.COLOR_BGR2HSV)[:,:,0].reshape(-1), bins=20, alpha=0.3)
+images = []
+images.append(['ceren_1.jpg', 'Ceren 1'])
+images.append(['ceren_2.jpg', 'Ceren 2'])
+images.append(['esra_1.jpg', 'Esra 1'])
+images.append(['esra_2.jpg', 'Esra 2'])
+
+names = {}
+for image, name in images:
+    plt.figure(figsize=(16,12)); plt.title(name + ' ' + histogramType + ' Histogram'); plt.xlabel('Data Points'); plt.ylabel('Occurrences')
+    hist = plt.hist(cv2.cvtColor(cv2.imread(image), cv2.COLOR_BGR2HSV)[:,:,0].reshape(-1), bins=bound, alpha=0.6)
+    hist = hist[0] / np.sum(hist[0])
+
+    names[name] = hist
+
+# Error calculations...
+#names = {'Ceren 1' : ceren1_hist, 'Ceren 2' : ceren2_hist, 'Esra 1' : esra1_hist, 'Esra 2' : esra2_hist}
+for name1 in names:
+    for name2 in names:
+        if name1 == name2:
+            continue
+
+        print('Cost from {0:10s} to {1:10s} = {2:6f}'.format(name1, name2, np.sum(np.abs(names[name1] - names[name2]))))
+
+
 
 plt.show()
-
-

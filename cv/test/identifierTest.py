6bcb33ee87b6cee4848a917fc8c052d35d12fdce
Fatih Eser <afeserr@gmail.com>
2020-05-19 19:55:07 +0300

    remove old test...

--- cv/test/identifierTest.py
+++ cv/test/identifierTest.py
@@ -13,65 +13,7 @@ Tester Siftir class
 To start it, place it to the main directory by copying and start using python3
 '''
 
-work_dir = 'cv/data/SIFT'
-logging.basicConfig(filename='identifier_test_log.txt', level=logging.DEBUG)
-
-def test1():
-    # Old test for saveCat, not used anymore...
-
-
-    ## Load sample images
-    im1 = cv2.imread(work_dir + '/resim.jpg')
-    doga1 = cv2.imread(work_dir + '/doga1_low.jpg')
-    doga2 = cv2.imread(work_dir + '/doga2_low.jpg')
-    doga3 = cv2.imread(work_dir + '/doga3_low.jpg')
-    doga4 = cv2.imread(work_dir + '/doga4_low.jpg')
-    doga5 = cv2.imread(work_dir + '/doga5_low.jpg')
-    utku1 = cv2.imread(work_dir + '/utku1_low.jpg')
-    utku2 = cv2.imread(work_dir + '/utku2_low.jpg')
-    utku3 = cv2.imread(work_dir + '/utku3_low.jpg')
-    utku4 = cv2.imread(work_dir + '/utku4_low.jpg')
-    utku5 = cv2.imread(work_dir + '/utku5_low.jpg')
-
-    doga1High = cv2.imread(work_dir + '/doga1.jpg')
-    doga2High = cv2.imread(work_dir + '/doga2.jpg')
-    doga1crop = cv2.imread(work_dir + '/doga1_crop.jpg')
-    doga2crop = cv2.imread(work_dir + '/doga2_crop.jpg')
-
-    ## Class tests...
-    a = Identifier()
-    # a._databaseDir = 'cikti'
-
-    a.saveCat(doga1, 'doga')
-    a.saveCat(doga2, 'doga')
-    a.saveCat(doga3, 'doga')
-    a.saveCat(doga4, 'doga')
-    a.saveCat(utku1, 'utku')
-    a.saveCat(utku2, 'utku')
-    a.saveCat(utku3, 'utku')
-    a.saveCat(utku4, 'utku')
-    # a.saveCat(im1, 'rainbow')
-
-
-    ## Visual investigation
-    if not os.path.exists(work_dir + '/TEST'):
-        os.makedirs(work_dir + '/TEST', exist_ok=True)
-    # a.saveSIFTImage(doga1, work_dir + '/TEST/LowDogaOutput.jpg')
-    # a.saveSIFTImage(doga1High, work_dir + '/TEST/HighDogaOutput.jpg')
-    # a.saveSIFTImage(doga2High, work_dir + '/TEST/HighDogaOutput2.jpg')
-    # a.saveSIFTImage(doga1crop, work_dir + '/TEST/Doga1Crop.jpg')
-    # a.saveSIFTImage(doga2crop, work_dir + '/TEST/Doga2Crop.jpg')
-
-
-
-    b = Identifier()
-    # b.resetDatabase(force=True)
-    b.importDirectory(work_dir + '/dataset')
-    sys.exit(0)
-
-    print(a.getCatId(doga5))
-    print(a.getCatId(utku5))
-    print(a.getCatId(im1))
+logging.basicConfig(filename='identifier_test_log.txt', level=logging.NOTSET)
 
 def train_validation_test_accuracy(train_root_name, validation_root_name, test_root_name, new_cat_labels, databaseLocation=None):
     '''
@@ -169,7 +111,7 @@ def train_validation_test_accuracy(train_root_name, validation_root_name, test_r
 
     return a
 
-def test2():
+def train_validation_test_bench():
     '''
     18 May 2020 Utku Serhan Database Test V1
     '''
@@ -179,4 +121,4 @@ def test2():
     new_cat_labels       = ['07', '08']
     train_validation_test_accuracy(train_root_name, validation_root_name, test_root_name, new_cat_labels, 'database_utku_V1')
 
-test2()
+
